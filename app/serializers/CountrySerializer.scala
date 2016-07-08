c48f7ef3bde773662ef77de38a1e8a078e81cfc1
Kiril Dokh <kiril.dokh@onapp.com>
2016-07-08 22:44:58 +0300

    add Try to parser

--- app/serializers/CountrySerializer.scala
+++ app/serializers/CountrySerializer.scala
@@ -1,13 +1,17 @@
 package serializers
 
+import scala.util.Try
+
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import models._
 
 object CountryRequestSerializer {
+  val DefaultValue = ""
+
   def toModel(request: JsValue) = {
-    val title = (request \ "title").as[String]
-    val abbreviation = (request \ "abbreviation").as[String]
+    val title = Try((request \ "title").as[String]).getOrElse(DefaultValue)
+    val abbreviation = Try((request \ "abbreviation").as[String]).getOrElse(DefaultValue)
 
     new Country(0, title, abbreviation, true)
   }
