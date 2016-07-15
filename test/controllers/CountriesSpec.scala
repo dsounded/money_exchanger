a0857b5b017243e02617b48bc1e9914b02760189
Kiril Dokh <kiril.dokh@onapp.com>
2016-07-15 12:30:21 +0300

    Fix the specs, remove helpers for test app

--- test/controllers/CountriesSpec.scala
+++ test/controllers/CountriesSpec.scala
@@ -1,9 +1,25 @@
-import org.scalatestplus.play._
-import play.api.test._
+import org.scalatest.BeforeAndAfterAll
+import org.scalatestplus.play.PlaySpec
+
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
 import play.api.test.Helpers._
+
 import play.api.libs.json._
 
-class CountriesSpec extends PlaySpec with OneAppPerTest {
+import test.helpers.DatabaseCleaner
+
+import play.api.Play
+
+class CountriesSpec extends PlaySpec with BeforeAndAfterAll {
+  val app = new GuiceApplicationBuilder().build
+
+  override def beforeAll() {
+    Play.start(app)
+
+    DatabaseCleaner.clean(List("Countries"))
+  }
+
   "Countries" should {
     "render the index page" in {
       val index = route(app, FakeRequest(GET, "/countries")).get
@@ -14,10 +30,11 @@ class CountriesSpec extends PlaySpec with OneAppPerTest {
     }
 
     "saves the record on create" in {
+      //DatabaseCleaner.clean(List("Countries"))
       val request = FakeRequest(POST, "/countries").withJsonBody(Json.parse("""{ "country": {"title":"Germany", "abbreviation":"GER"} }"""))
       val create = route(app, request).get
 
-      status(create) mustBe OK
+      status(create) mustBe CREATED
       contentType(create) mustBe Some("application/json")
       contentAsString(create) must include("country")
     }
